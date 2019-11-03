e7c55b0b1afcbb25a0027c7674c05aeefe01059b
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2019-11-03 21:13:40 +0200

    Updates

---
+++ precache-manifest.17bd6da0d15202a184077bab81e01248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138d6e1ac613d45a70766686e1604bb7",
+    "url": "/goit-react-hw-03-image-finder/./index.html"
+  },
+  {
+    "revision": "cf66a9ff4aad32d15ea1",
+    "url": "/goit-react-hw-03-image-finder/./static/css/main.3c14251c.chunk.css"
+  },
+  {
+    "revision": "1c8007af5cfb0e0fa603",
+    "url": "/goit-react-hw-03-image-finder/./static/js/2.c1522b86.chunk.js"
+  },
+  {
+    "revision": "cf66a9ff4aad32d15ea1",
+    "url": "/goit-react-hw-03-image-finder/./static/js/main.9a6f7742.chunk.js"
+  },
+  {
+    "revision": "67014c74e12e26dabe77",
+    "url": "/goit-react-hw-03-image-finder/./static/js/runtime-main.98ade033.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%20//kozachenko1983.github.io/goit-react-hw-03-image-finder/./precache-manifest.371e95fe5571d6f159206663c95b64b5.js"
+  "/goit-react-hw-03-image-finder/./precache-manifest.17bd6da0d15202a184077bab81e01248.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%20//kozachenko1983.github.io/goit-react-hw-03-image-finder/./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-image-finder/./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
